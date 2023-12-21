79896603c64e8952d71168c0ac0f53111784a780
bilawal316 <mbilawalzaman@gmail.com>
2023-12-21 08:23:15 +0500

    new update express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var app = express();
 
@@ -12,6 +13,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors()); 
+
 
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const userController = require("../controllers/userController");
 const { trainee, instructor, admin } = require("../middleware");
 const router = require("express").Router();
 
-router.post("/createUser",instructor, userController.createUser);
+router.post("/createUser", userController.createUser);
 router.get("/getAllUsers", trainee, userController.getAllUser);
 router.delete("/deleteUser", instructor, userController.deleteUser);
 router.put("/updateUser", admin, userController.updateUser);
